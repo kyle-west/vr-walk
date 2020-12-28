37f81555a8216481da1bb54be353d136366b54b7
Kyle West <kyle@familysearch.org>
2020-12-28 11:09:56 -0700

    [clipping] test out custom clipper to remove OOV elements

--- src/layout.js
+++ src/layout.js
@@ -1,29 +1,52 @@
 import { ImageWall } from './image-wall.js'
 import { addToWorld } from './env.js'
 
+class Clipper {
+  constructor (elem, visFn = () => false) {
+    this.elem = elem
+    this.pos = { x:0, y:0, z:0 }
+    this.getVisible = visFn
+    document.addEventListener('player:position', this.positionChanged.bind(this))
+  }
+
+  set visible (vis) {
+    if (vis !== this._visible) {
+      this._visible = vis
+      this.elem.setAttribute('visible', this._visible)
+    }
+  }
+
+  positionChanged ({detail: {x,y,z}}) {
+    this.visible = this.getVisible({ x,y,z, elem: this.elem })
+  }
+}
+
+
 export function generateImageWalls (images) {
   if (images.length < 18) {
     const lightIntensity = 0.075
     const h1 = [...images]
     const h2 = h1.splice(0, Math.floor(h1.length / 2))
     
-    addToWorld(
-      ImageWall({ images: h1, rotation: '0 90 0', position: '3 0 -3', lightIntensity }),
-      ImageWall({ images: h2, rotation: '0 270 0', position: '-8 0 3', lightIntensity })
-      )
+    const east = ImageWall({ images: h1, rotation: '0 90 0', position: '3 0 -3', lightIntensity })
+    const west = ImageWall({ images: h2, rotation: '0 270 0', position: '-8 0 3', lightIntensity })
+
+    addToWorld(east, west)
   } else {
     const lightIntensity = images.length > 25 ? 0.05 : 0.075
     const h1 = [...images]
     const h2 = h1.splice(0, Math.floor(h1.length / 2))
     const h3 = h1.splice(0, Math.ceil(h1.length * 1 / 3))
     const h4 = h2.splice(0, Math.ceil(h2.length * 1 / 3))
-  
-    addToWorld(
-      ImageWall({ images: h1, rotation: '0 90 0', position: '3 0 -3', lightIntensity }),
-      ImageWall({ images: h2, rotation: '0 270 0', position: '-8 0 3', lightIntensity }),
 
-      ImageWall({ images: h3, rotation: '0 180 0', position: `3.13 0 -${h3.length * 2 + 3.13}`, lightIntensity }),
-      ImageWall({ images: h4, rotation: '0 0 0', position: `-8.13 0 ${h4.length * 2 + 3.13}`, lightIntensity }),
-    )
+    const east = ImageWall({ images: h1, rotation: '0 90 0', position: '3 0 -3', lightIntensity })
+    const west = ImageWall({ images: h2, rotation: '0 270 0', position: '-8 0 3', lightIntensity })
+    const south = ImageWall({ images: h3, rotation: '0 180 0', position: `3.13 0 -${h3.length * 2 + 3.13}`, lightIntensity })
+    const north = ImageWall({ images: h4, rotation: '0 0 0', position: `-8.13 0 ${h4.length * 2 + 3.13}`, lightIntensity })
+  
+    new Clipper(south, ({ x }) => x >= 3)
+    new Clipper(north, ({ x }) => x <= -8)
+
+    addToWorld(north, south, east, west)
   }
 }
--- src/scene.js
+++ src/scene.js
@@ -4,6 +4,24 @@ import { leftHand, rightHand } from './controllers.js'
 import environment from './env.js'
 import { generateImageWalls } from './layout.js'
 
+// Fire DOM event when camera position changes
+(() => {
+  let x,y,z;
+
+  AFRAME.registerComponent('camera-listener', {
+    tick: function () {
+      const cameraEl = this.el.sceneEl.camera.el;
+      const { x:dx, y:dy, z:dz } = cameraEl.getAttribute('position');
+      
+      if (x !== dx || y !== dy || z !== dz) {
+        x = dx; y = dy; z = dz;
+        document.dispatchEvent(new CustomEvent('player:position', { detail: { x,y,z }}))
+      }
+    }
+  });
+})()
+
+
 const scene = make({ type: 'scene' })
 const cameraRig = make({ id: 'cameraRig', type: 'entity' })
 
@@ -13,7 +31,7 @@ export function addToScene (...nodes) {
 
 addToScene(assets, environment, cameraRig)
 
-const camera = make({ id: 'head', type: 'entity', position: "0 1.5 0", camera: '', 'wasd-controls': '', 'look-controls': '' })
+const camera = make({ id: 'head', type: 'entity', position: "0 1.5 0", camera: '', 'wasd-controls': '', 'look-controls': '', 'camera-listener': '' })
 append(cameraRig, camera, leftHand(), rightHand())
 
 export class MediaViewer {
