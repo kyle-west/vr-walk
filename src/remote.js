42d479bda7d87a47792f1e9299b446662109c05e
Kyle West <kyle@familysearch.org>
2021-01-01 11:23:53 -0700

    [debug] remote stick controller

--- src/remote.js
+++ src/remote.js
@@ -1,8 +1,9 @@
 import { make } from './util.js'
+import { log } from './debug.js'
 
 window.remotes = window.remotes || {}
 
-export function Remote ({ name, color = 'white', btnColor = 'blue', actions, ...rest }) {
+export function Remote ({ name, color = 'red', btnColor = 'blue', actions, ...rest }) {
   const config = {
     type: 'box',
     id: `remote_${name}`,
@@ -34,11 +35,13 @@ export function Remote ({ name, color = 'white', btnColor = 'blue', actions, ...
     const hand = evt.detail.hand.id
     window.remotes[name] = { active: true, hand, actions }
     hand && window.controllers[hand].hideMesh()
+    log(`<${name}>::grab-start[${hand}] active=false`)
   })
   remote.addEventListener('grab-end', (evt) => {
     const hand = evt.detail.hand.id
     window.remotes[name] = { active: false, hand, actions }
     hand && window.controllers[hand].restoreMesh()
+    log(`<${name}>::grab-end[${hand}] active=false`)
   })
   
   remote.appendChild(topBtn)
